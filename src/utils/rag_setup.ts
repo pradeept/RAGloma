474ed1bab5904fd31d303a6ba930bb808f49ed6b
pradeept <pradeep.tarakar17@gmail.com>
2025-09-16 11:25:28 +0530

    feat: add embedding model and vectorstore

--- src/utils/rag_setup.ts
+++ src/utils/rag_setup.ts
@@ -1,6 +1,9 @@
 import path from "path";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { OllamaEmbeddings } from "@langchain/ollama";
+import { MongoDBAtlasVectorSearch } from "@langchain/mongodb";
+import { MongoClient } from "mongodb";
 
 export async function ragSetup() {
   const pdfPath = path.join(process.cwd(), "public/nke-10k-2023.pdf");
@@ -15,5 +18,62 @@ export async function ragSetup() {
 
   const allSplits = await textSplitter.splitDocuments(pages);
 
-  console.log(allSplits[1]);
+  // ---- Embeddings ----
+  const embeddings = new OllamaEmbeddings({
+    model: process.env.EMBEDDING_MODEL,
+    baseUrl: process.env.OLLAMA_BASE_URL,
+  });
+
+  // ---- Vector Store ----
+  const client = new MongoClient(process.env.MONGODB_ATLAS_URI || "");
+  await client.connect().catch((e) => console.error(e));
+
+  const collection = client
+    .db(process.env.MONGODB_ATLAS_DB_NAME)
+    .collection(process.env.MONGODB_ATLAS_COLLECTION_NAME!);
+
+  const vectorStore = new MongoDBAtlasVectorSearch(embeddings, {
+    collection: collection,
+    indexName: "vector_index",
+    textKey: "text",
+    embeddingKey: "embedding",
+  });
+
+  // --- Add docs ---
+  await vectorStore.addDocuments(allSplits).catch((e) => console.error(e));
+
+  // Step 2: Example: Search for similar chunks
+  const query = "What was Nike's revenue in 2023?";
+  const results = await vectorStore.similaritySearch(query, 5);
+
+  results.forEach((doc, i) => {
+    console.log(`Result ${i + 1}:`);
+    console.log(doc.pageContent);
+  });
+  // console.log(allSplits[1].metadata);
 }
+
+/*
+Error
+
+[TypeError: fetch failed] {
+  [cause]: [Error [HeadersTimeoutError]: Headers Timeout Error] {
+    code: 'UND_ERR_HEADERS_TIMEOUT'
+  }
+}
+ ⨯ unhandledRejection: [TypeError: fetch failed] {
+  [cause]: [Error [HeadersTimeoutError]: Headers Timeout Error] {
+    code: 'UND_ERR_HEADERS_TIMEOUT'
+  }
+}
+ ⨯ unhandledRejection:  [TypeError: fetch failed] {
+  [cause]: [Error [HeadersTimeoutError]: Headers Timeout Error] {
+    code: 'UND_ERR_HEADERS_TIMEOUT'
+  }
+}
+[TypeError: fetch failed] {
+  [cause]: [Error [HeadersTimeoutError]: Headers Timeout Error] {
+    code: 'UND_ERR_HEADERS_TIMEOUT'
+  }
+}
+*/
